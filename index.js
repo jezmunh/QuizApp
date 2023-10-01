49482ff4ceab5153f7ae8741f5f2da851a6d8f37
jezmunh <em.danila@yandex.ru>
2023-10-01 19:55:48 +0200

    smth

--- index.js
+++ index.js
@@ -27,14 +27,39 @@ function startProgram() {
 }
 function generateQuestion() {
     let question = allQuestions[currentQuestions].question
-    main.innerHTML = `<h1 class="question">${question}</h1>`
+    main.innerHTML = `<h1 class="my-5" style="font-size:100px !important">${question}</h1>`
 
     let answers = allQuestions[currentQuestions].answers
     let btn_block = ""
     for (let i of answers) {
-        btn_block += `<button class="btn" onclick="checkQuestion(${i})">${i}</button>`
+        btn_block += `<button class="btn btn-outline-primary fs-1" onclick="checkQuestion(${i})">${i}</button>`
         
     }
-    main.innerHTML += `<nav class="btn_block">${btn_block}</button>`
+    main.innerHTML += `<nav class="d-flex justify-content-between" style="width:50vw !important">${btn_block}</button>`
 
 }
+
+function checkQuestion(num) {
+    let correct = allQuestions[currentQuestions].correct
+    if (num == correct) {
+        countSuccess += 1
+    }
+    currentQuestions += 1
+    if (allQuestions.length > currentQuestions) {
+        generateQuestion()
+    } else {
+        stopQuiz()
+    }
+
+
+}
+
+function stopQuiz() {
+    start.classList.remove('none')
+    main.classList.add('none')
+    result.classList.remove('none')
+    result.innerHTML = `Результат ${countSuccess} из ${allQuestions.length}`
+
+    currentQuestions = 0
+    countSuccess = 0
+} 
